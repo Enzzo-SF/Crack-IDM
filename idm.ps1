a9e3d78b28185cae3fd7234e15a83576c761ca96
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-04-25 13:42:55 +0700

    Update idm.ps1

--- idm.ps1
+++ idm.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/ictcentervnn/CMD-WIN-IDM/main/idm.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/BsNgChiThanh/Crack-IDM/IMP/IDM.cmd'
 
 $rand = Get-Random -Maximum 1000
 $FilePath = "$env:TEMP\idm_$rand.cmd"
